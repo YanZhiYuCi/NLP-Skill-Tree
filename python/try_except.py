94a8aad77bae69edbc046ace239d220a9e8a68aa
machaoyang <2328414576@qq.com>
2023-02-27 19:55:35 +0800

    feat:try_except中增加示例

--- python/try_except.py
+++ python/try_except.py
@@ -28,3 +28,28 @@ try:
     print('345')
 except Exception as e:
     print('012')
+
+print('===================================================================================')
+try:
+    print('123')
+    c1
+except Exception as e:
+    try:
+        print('456')
+        c2
+    except Exception as e:
+        print('789')
+    print('012')
+    print(e)
+
+try:
+    print('123')
+    c1
+except Exception as e:
+    try:
+        print('456')
+        c2
+    except Exception as e:
+        print('789')
+        print(e)
+    print('012')
