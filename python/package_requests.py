26f0c835892a24b9c2bc9821c88e486159c550ab
machaoyang <2328414576@qq.com>
2023-02-28 16:08:45 +0800

    feat:requests示例

--- python/package_requests.py
+++ python/package_requests.py
@@ -9,7 +9,7 @@ print(type(r.json()))
 kw = {'s': 'python 教程'}
 # 设置请求头
 headers = {"User-Agent":
-               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
 # params 接收一个字典或者字符串的查询参数，字典类型自动转换为url编码，不需要urlencode()
 r = requests.get("https://www.runoob.com/", params=kw, headers=headers)
 print(r.text)
