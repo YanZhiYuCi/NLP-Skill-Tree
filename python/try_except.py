af29389eabe66d68ff1b2cea3530475db0e35ad6
machaoyang <2328414576@qq.com>
2023-02-28 10:06:56 +0800

    refactor:try_except增加注释

--- python/try_except.py
+++ python/try_except.py
@@ -1,55 +1,60 @@
+import traceback
+
+print('========================================a===========================================')
 try:
-    print('123')
+    print('a123')
     c1
 except Exception as e:
-    print('456')
-print('789')
+    traceback.print_exc()  # 打印try语句中出现的异常
+    print('a456')
+print('a789')
 
-print('===================================================================================')
+print('========================================b===========================================')
 try:
-    print('123')
-    c1
-except Exception as e:
-    try:
-        print('456')
-        c2
-    except Exception as e:
-        print('789')
-    print('012')
-
-print('===================================================================================')
-try:
-    print('123')
-    try:
-        print('456')
-        c2
-    except Exception as e:
-        print('789')
-    print('345')
-except Exception as e:
-    print('012')
-
-print('===================================================================================')
-try:
-    print('123')
+    print('b123')
     c1
 except Exception as e:
     try:
-        print('456')
+        print('b456')
         c2
     except Exception as e:
-        print('789')
-    print('012')
-    print(e)
+        print('b789')
+    print('b012')
 
+print('=======================================c============================================')
 try:
-    print('123')
+    print('c123')
+    try:
+        print('c456')
+        c2
+    except Exception as e:
+        print('c789')
+    print('c345')
+except Exception as e:
+    print('c012')
+
+print('=======================================d============================================')
+try:
+    print('d123')
     c1
 except Exception as e:
     try:
-        print('456')
+        print('d456')
         c2
     except Exception as e:
-        print('789')
+        print('d789')
+    print('d012')
+    print(e)  # 这里会报错
+
+print('=======================================e============================================')
+try:
+    print('e123')
+    c1
+except Exception as e:
+    try:
+        print('e456')
+        c2
+    except Exception as e:
+        print('e789')
         print(e)
-    print('012')
+    print('e012')
