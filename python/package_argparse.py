206eb9a0a6031f14c5f641937af795e9a82f3de3
machaoyang <2328414576@qq.com>
2023-03-08 09:51:39 +0800

    feat:增加argparse使用__dict__属性获取实例属性

--- python/package_argparse.py
+++ python/package_argparse.py
@@ -7,4 +7,7 @@ parser.add_argument('--epochs', type=int, default=5000, help='Train epochs.')
 parser.add_argument('--batchSize', type=int, default=15, help='Train batch size.')
 configs = parser.parse_args()
 # 此时得到的configs为Namespace类型，它包含了__dict__属性，因此可以使用vars将configs转换为字典dic（只要变量包含了__dict__属性就可以使用vars转换为字典）
+# 或者直接使用configs.__dict__
 dic = vars(configs)
+c = configs.__dict__
+print()
