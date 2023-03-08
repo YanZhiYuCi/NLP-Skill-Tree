fa367a72d6bf009a4e1e9ab42196e3c8d0ec2957
machaoyang <2328414576@qq.com>
2023-03-08 18:32:21 +0800

    feat:在windows上跑通ner

--- algorithm/src/package_simpletransformers/examples/named_entity_recognition/named_entity_recognition.py
+++ algorithm/src/package_simpletransformers/examples/named_entity_recognition/named_entity_recognition.py
@@ -5,53 +5,31 @@ from scipy.special import softmax
 from simpletransformers.ner import NERModel
 
 # Creating train_df  and eval_df for demonstration
-train_data = [
-    [0, "Simple", "B-MISC"],
-    [0, "Transformers", "I-MISC"],
-    [0, "started", "O"],
-    [0, "with", "O"],
-    [0, "text", "O"],
-    [0, "classification", "B-MISC"],
-    [1, "Simple", "B-MISC"],
-    [1, "Transformers", "I-MISC"],
-    [1, "can", "O"],
-    [1, "now", "O"],
-    [1, "perform", "O"],
-    [1, "NER", "B-MISC"],
-]
+train_data = [[0, "Simple", "B-MISC"], [0, "Transformers", "I-MISC"], [0, "started", "O"], [0, "with", "O"],
+              [0, "text", "O"], [0, "classification", "B-MISC"], [1, "Simple", "B-MISC"], [1, "Transformers", "I-MISC"],
+              [1, "can", "O"], [1, "now", "O"], [1, "perform", "O"], [1, "NER", "B-MISC"], ]
 train_df = pd.DataFrame(train_data, columns=["sentence_id", "words", "labels"])
 
-eval_data = [
-    [0, "Simple", "B-MISC"],
-    [0, "Transformers", "I-MISC"],
-    [0, "was", "O"],
-    [0, "built", "O"],
-    [0, "for", "O"],
-    [0, "text", "O"],
-    [0, "classification", "B-MISC"],
-    [1, "Simple", "B-MISC"],
-    [1, "Transformers", "I-MISC"],
-    [1, "then", "O"],
-    [1, "expanded", "O"],
-    [1, "to", "O"],
-    [1, "perform", "O"],
-    [1, "NER", "B-MISC"],
-]
+eval_data = [[0, "Simple", "B-MISC"], [0, "Transformers", "I-MISC"], [0, "was", "O"], [0, "built", "O"],
+             [0, "for", "O"], [0, "text", "O"], [0, "classification", "B-MISC"], [1, "Simple", "B-MISC"],
+             [1, "Transformers", "I-MISC"], [1, "then", "O"], [1, "expanded", "O"], [1, "to", "O"], [1, "perform", "O"],
+             [1, "NER", "B-MISC"], ]
 eval_df = pd.DataFrame(eval_data, columns=["sentence_id", "words", "labels"])
 
 # Create a NERModel
 model = NERModel(
     "bert",
     "bert-base-cased",
-    args={"overwrite_output_dir": True, "reprocess_input_data": True},
+    args={"overwrite_output_dir": True, "reprocess_input_data": True, "use_multiprocessing": False,
+          "dataloader_num_workers": 0, "process_count": 1, "use_multiprocessing_for_evaluation": False},
+    use_cuda=False
 )
 
-# # Train the model
-# model.train_model(train_df)
+# Train the model
+model.train_model(train_df)
 
-# # Evaluate the model
-# result, model_outputs, predictions = model.eval_model(eval_df)
-
+# Evaluate the model
+result, model_outputs, predictions = model.eval_model(eval_df)
 
 # Predictions on arbitary text strings
 sentences = ["Some arbitary sentence", "Simple Transformers sentence"]
