46e944e94adac22975fb296ed139ffe17271fd49
machaoyang <2328414576@qq.com>
2023-04-11 10:50:11 +0800

    feat:增加enumerate zip混用示例

--- python/for_else.py
+++ python/for_else.py
@@ -23,3 +23,8 @@ for x in range(2, n):
 else:
     # loop fell through without finding a factor
     print(n, 'is a prime number')
+
+predictions = [1, 2, 3]
+raw_outputs = [1.0, 2.0, 3.0]
+for n, (preds, outs) in enumerate(zip(predictions, raw_outputs)):
+    print(n, preds, outs)
