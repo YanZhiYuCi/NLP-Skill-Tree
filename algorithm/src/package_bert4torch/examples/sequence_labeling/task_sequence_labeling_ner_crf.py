586700dcc230959a112145a722f013fd9ed2996b
machaoyang <2328414576@qq.com>
2023-04-26 16:05:46 +0800

    feat:bert4torch中bert+crf的示例

--- algorithm/src/package_bert4torch/examples/sequence_labeling/task_sequence_labeling_ner_crf.py
+++ algorithm/src/package_bert4torch/examples/sequence_labeling/task_sequence_labeling_ner_crf.py
@@ -22,13 +22,19 @@ categories_id2label = {i: k for i, k in enumerate(categories)}
 categories_label2id = {k: i for i, k in enumerate(categories)}
 
 # BERT base
-config_path = 'F:/Projects/pretrain_ckpt/bert/[google_tf_base]--chinese_L-12_H-768_A-12/bert_config.json'
-checkpoint_path = 'F:/Projects/pretrain_ckpt/bert/[google_tf_base]--chinese_L-12_H-768_A-12/pytorch_model.bin'
-dict_path = 'F:/Projects/pretrain_ckpt/bert/[google_tf_base]--chinese_L-12_H-768_A-12/vocab.txt'
+# config_path = 'F:/Projects/pretrain_ckpt/bert/[google_tf_base]--chinese_L-12_H-768_A-12/bert_config.json'
+# checkpoint_path = 'F:/Projects/pretrain_ckpt/bert/[google_tf_base]--chinese_L-12_H-768_A-12/pytorch_model.bin'
+# dict_path = 'F:/Projects/pretrain_ckpt/bert/[google_tf_base]--chinese_L-12_H-768_A-12/vocab.txt'
+
+config_path = r'D:\Projects\machaoyangNLP\algorithm\src\package_bert4torch\data\bert-base-chinese\config.json'
+checkpoint_path = r'D:\Projects\machaoyangNLP\algorithm\src\package_bert4torch\data\bert-base-chinese\pytorch_model.bin'
+dict_path = r'D:\Projects\machaoyangNLP\algorithm\src\package_bert4torch\data\bert-base-chinese\vocab.txt'
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # 固定seed
 seed_everything(42)
+
 
 # 加载数据集
 class MyDataset(ListDataset):
@@ -55,6 +61,7 @@ class MyDataset(ListDataset):
 # 建立分词器
 tokenizer = Tokenizer(dict_path, do_lower_case=True)
 
+
 def collate_fn(batch):
     batch_token_ids, batch_labels = [], []
     for d in batch:
@@ -68,23 +75,30 @@ def collate_fn(batch):
             if start in start_mapping and end in end_mapping:
                 start = start_mapping[start]
                 end = end_mapping[end]
-                labels[start] = categories_label2id['B-'+label]
-                labels[start + 1:end + 1] = categories_label2id['I-'+label]
+                labels[start] = categories_label2id['B-' + label]
+                labels[start + 1:end + 1] = categories_label2id['I-' + label]
         batch_token_ids.append(token_ids)
         batch_labels.append(labels)
     batch_token_ids = torch.tensor(sequence_padding(batch_token_ids), dtype=torch.long, device=device)
     batch_labels = torch.tensor(sequence_padding(batch_labels), dtype=torch.long, device=device)
     return batch_token_ids, batch_labels
 
+
 # 转换数据集
-train_dataloader = DataLoader(MyDataset('F:/Projects/data/corpus/ner/china-people-daily-ner-corpus/example.train'), batch_size=batch_size, shuffle=True, collate_fn=collate_fn) 
-valid_dataloader = DataLoader(MyDataset('F:/Projects/data/corpus/ner/china-people-daily-ner-corpus/example.dev'), batch_size=batch_size, collate_fn=collate_fn) 
+train_path = r'D:\Projects\machaoyangNLP\algorithm\src\package_bert4torch\data\china-people-daily-ner-corpus\example.train'
+eval_path = r'D:\Projects\machaoyangNLP\algorithm\src\package_bert4torch\data\china-people-daily-ner-corpus\example.dev'
+train_dataloader = DataLoader(MyDataset(train_path)[0:10], batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+valid_dataloader = DataLoader(MyDataset(train_path)[0:10], batch_size=batch_size, collate_fn=collate_fn)
+
+
+# valid_dataloader = DataLoader(MyDataset(eval_path)[100:200], batch_size=batch_size, collate_fn=collate_fn)
 
 # 定义bert上的模型结构
 class Model(BaseModel):
     def __init__(self):
         super().__init__()
-        self.bert = build_transformer_model(config_path=config_path, checkpoint_path=checkpoint_path, segment_vocab_size=0)
+        self.bert = build_transformer_model(config_path=config_path, checkpoint_path=checkpoint_path,
+                                            segment_vocab_size=0)
         self.fc = nn.Linear(768, len(categories))  # 包含首尾
         self.crf = CRF(len(categories))
 
@@ -101,17 +115,21 @@ class Model(BaseModel):
             best_path = self.crf.decode(emission_score, attention_mask)  # [btz, seq_len]
         return best_path
 
+
 model = Model().to(device)
+
 
 class Loss(nn.Module):
     def forward(self, outputs, labels):
         return model.crf(*outputs, labels)
+
 
 def acc(y_pred, y_true):
     y_pred = y_pred[0]
     y_pred = torch.argmax(y_pred, dim=-1)
     acc = torch.sum(y_pred.eq(y_true)).item() / y_true.numel()
     return {'acc': acc}
+
 
 # 支持多种自定义metrics = ['accuracy', acc, {acc: acc}]均可
 model.compile(loss=Loss(), optimizer=optim.Adam(model.parameters(), lr=2e-5), metrics=acc)
@@ -136,7 +154,7 @@ def evaluate(data):
         Y2 += len(entity_pred)
         Z2 += len(entity_true)
     f1, precision, recall = 2 * X / (Y + Z), X / Y, X / Z
-    f2, precision2, recall2 = 2 * X2 / (Y2 + Z2), X2/ Y2, X2 / Z2
+    f2, precision2, recall2 = 2 * X2 / (Y2 + Z2), X2 / Y2, X2 / Z2
     return f1, precision, recall, f2, precision2, recall2
 
 
@@ -152,7 +170,7 @@ def trans_entity2tuple(scores):
                 entity_ids.append([i, j, j, flag_tag[2:]])
             elif len(entity_ids) == 0:
                 continue
-            elif (len(entity_ids[-1]) > 0) and flag_tag.startswith('I-') and (flag_tag[2:]==entity_ids[-1][-1]):  # I
+            elif (len(entity_ids[-1]) > 0) and flag_tag.startswith('I-') and (flag_tag[2:] == entity_ids[-1][-1]):  # I
                 entity_ids[-1][-2] = j
             elif len(entity_ids[-1]) > 0:
                 entity_ids.append([])
@@ -162,9 +180,11 @@ def trans_entity2tuple(scores):
                 batch_entity_ids.add(tuple(i))
     return batch_entity_ids
 
+
 class Evaluator(Callback):
     """评估与保存
     """
+
     def __init__(self):
         self.best_val_f1 = 0.
 
@@ -174,14 +194,16 @@ class Evaluator(Callback):
             self.best_val_f1 = f2
             # model.save_weights('best_model.pt')
         print(f'[val-token  level] f1: {f1:.5f}, p: {precision:.5f} r: {recall:.5f}')
-        print(f'[val-entity level] f1: {f2:.5f}, p: {precision2:.5f} r: {recall2:.5f} best_f1: {self.best_val_f1:.5f}\n')
+        print(
+            f'[val-entity level] f1: {f2:.5f}, p: {precision2:.5f} r: {recall2:.5f} best_f1: {self.best_val_f1:.5f}\n')
 
 
 if __name__ == '__main__':
 
+    num_epoch = 50
     evaluator = Evaluator()
-    model.fit(train_dataloader, epochs=20, steps_per_epoch=None, callbacks=[evaluator])
+    model.fit(train_dataloader, epochs=num_epoch, steps_per_epoch=None, callbacks=[evaluator])
 
-else: 
+else:
 
     model.load_weights('best_model.pt')
