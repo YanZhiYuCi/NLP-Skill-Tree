fd29436a4b075e9e62df3323e4c211442dd3887f
machaoyang <2328414576@qq.com>
2023-05-08 15:34:21 +0800

    refactor:队列示例

--- python/package_多线程和多进程/demo_Queue.py
+++ python/package_多线程和多进程/demo_Queue.py
@@ -1,49 +1,9 @@
 import queue
 
 q = queue.Queue()
-for i in [1] * 10:
+data = list(range(10))
+for i in data:
     q.put(i)
-print(q.qsize())
-print(q)
+print('q.qsize:{}'.format(q.qsize()))
 while not q.empty():
-    print(q.get())
-
-# ######################################################################################################################
-import queue
-import threading
-
-q = queue.Queue()
-
-
-def worker():
-    while True:
-        item = q.get()
-        print(f'Working on {item}')
-        print(f'Finished {item}')
-        q.task_done()
-
-
-# turn-on the worker thread
-threading.Thread(target=worker, daemon=True).start()
-
-# send thirty task requests to the worker
-for item in range(30):
-    q.put(item)
-print('All task requests sent\n', end='')
-
-# block until all tasks are done
-q.join()
-print('All work completed')
-
-# 多进程#################################################################################################################
-from multiprocessing import Process
-
-
-def f(name):
-    print('hello', name)
-
-
-if __name__ == '__main__':
-    p = Process(target=f, args=('bob',))
-    p.start()
-    p.join()
+    print('q.get:{}'.format(q.get()))
